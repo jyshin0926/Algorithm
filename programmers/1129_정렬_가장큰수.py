c6b4151417c9763ef921d3c493ae57805ac9b63b
cindy <sjy777star@gmail.com>
2020-11-29 21:48:16 +0900

    코딩테스트연습_정렬_가장큰수 https:/programmers.co.kr/learn/courses/30/lessons/42746
    문제 설명
    0 또는 양의 정수가 주어졌을 때, 정수를 이어 붙여 만들 수 있는 가장 큰 수를 알아내 주세요.
    
    예를 들어, 주어진 정수가 [6, 10, 2]라면 [6102, 6210, 1062, 1026, 2610, 2106]를 만들 수 있고, 이중 가장 큰 수는 6210입니다.
    
    0 또는 양의 정수가 담긴 배열 numbers가 매개변수로 주어질 때, 순서를 재배치하여 만들 수 있는 가장 큰 수를 문자열로 바꾸어 return 하도록 solution 함수를 작성해주세요.
    
    제한 사항
    numbers의 길이는 1 이상 100,000 이하입니다.
    numbers의 원소는 0 이상 1,000 이하입니다.
    정답이 너무 클 수 있으니 문자열로 바꾸어 return 합니다.

---
+++ programmers/1129_정렬_가장큰수.py
@@ -0,0 +1,31 @@
+# 0 또는 양의 정수가 담긴 배열 numbers가 매개변수
+# 문자열로 바꿔서 리턴
+# numbers의 길이는 1 이상 100,000 이하입니다.
+# numbers의 원소는 0 이상 1,000 이하입니다.
+
+# 테스트케이스 통과 / permutation 시간초과
+from itertools import permutations
+def solution1(numbers):
+    num = [str(x) for x in numbers]
+    tmp = [int(x) for x in list(map(''.join, permutations(num)))]
+    return str(max(tmp))
+
+# 통과
+def solution(numbers):
+    num = [str(x) for x in numbers]  # 문자열 비교를 위해 아스키코드로 정렬되는 sort 사용
+    num.sort(key=lambda x: x*3, reverse=True)  # 원소들이 1000 이하이므로 최소 3자리까지 비교 가능하도록 하기
+    return str(int(''.join(num)))  # '000' 처리
+
+
+print(solution([6, 10, 2]))
+print(solution([3, 30, 34, 5, 9]))
+
+print(ord('0'))
+print(ord('2'))
+print(ord('3'))
+
+print(sorted(['333','30','3','1000'],key=lambda x:x*2, reverse=True))
+print(sorted(['30'*3,'3'*3],reverse=True))
+print(sorted(['30','3'],reverse=True))
+print(sorted(['30'*2,'3'*2],reverse=True))
+print(sorted(['adad','abcabc'],reverse=True))
