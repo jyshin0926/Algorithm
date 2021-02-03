981da4722ce7e06b3902040e11f714f733fee902
jyshin0926 <sjy777star@gmail.com>
2021-02-04 00:56:53 +0900

    210204_Search_IceCreamParlor https://www.hackerrank.com/challenges/icecream-parlor/problem
    Sunny and Johnny like to pool their money and go to the ice cream parlor. Johnny never buys the same flavor that Sunny does. The only other rule they have is that they spend all of their money.
    
    Given a list of prices for the flavors of ice cream, select the two that will cost all of the money they have.
    
    For example, they have m=6 to spend and there are flavors costing cost=[1,3,4,5,6]. The two flavors costing 1 and 5 meet the criteria. Using 1-based indexing, they are at indices 1 and 4.
    
    Function Description
    
    Complete the icecreamParlor function in the editor below. It should return an array containing the indices of the prices of the two flavors they buy, sorted ascending.
    
    icecreamParlor has the following parameter(s):
    
    m: an integer denoting the amount of money they have to spend
    cost: an integer array denoting the cost of each flavor of ice cream

--- HackerRank/Search_IceCreamParlor.py
+++ HackerRank/Search_IceCreamParlor.py
@@ -9,6 +9,6 @@ print(icecreamParlor(4,[2,2,4,3]))
 
 # t: ice cream parlor에 가는 횟수
 # m: 쓸 비용
-# cost : 각 맛 아이스크림의 n만큼의 array인데 i=1 ~ n까지이고 각 i번째 flavor의 cost
+# cost : 각 i번째 flavor의 cost
 # n : 한 번에 제공되는 맛
 # 맞는 cost에 해당되는 flavor의 Index+1 리턴(오름순으로 차례대로)
