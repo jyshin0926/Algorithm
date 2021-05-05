18247d0b1e643d2c78cd7c031bcdb7184009d195
jyshin0926 <sjy777star@gmail.com>
2021-05-05 21:51:10 +0900

    210505_DFS&graph_동굴탐험 https://programmers.co.kr/learn/courses/30/lessons/67260
    문제 설명
    [본 문제는 정확성과 효율성 테스트 각각 점수가 있는 문제입니다.]
    
    오지 탐험가인 프로도는 탐험 도중 n개의 방으로 이루어진 지하 동굴을 탐험하게 되었습니다. 모든 방에는 0부터 n - 1 까지 번호가 붙어있고, 이 동굴에 들어갈 수 있는 유일한 입구는 0번 방과 연결되어 있습니다. 각 방들은 양방향으로 통행이 가능한 통로로 서로 연결되어 있는데, 서로 다른 두 방을 직접 연결하는 통로는 오직 하나입니다. 임의의 서로 다른 두 방 사이의 최단경로는 딱 한 가지만 있으며, 또한 임의의 두 방 사이에 이동이 불가능한 경우는 없습니다.
    
    탐험에 앞서 이 지하 동굴의 지도를 손에 넣은 프로도는 다음과 같이 탐험 계획을 세웠습니다.
    
    모든 방을 적어도 한 번은 방문해야 합니다.
    특정 방은 방문하기 전에 반드시 먼저 방문할 방이 정해져 있습니다.
    2-1. 이는 A번 방은 방문하기 전에 반드시 B번 방을 먼저 방문해야 한다는 의미입니다.
    2-2. 어떤 방을 방문하기 위해 반드시 먼저 방문해야 하는 방은 없거나 또는 1개 입니다.
    2-3. 서로 다른 두 개 이상의 방에 대해 먼저 방문해야 하는 방이 같은 경우는 없습니다.
    2-4. 어떤 방이 먼저 방문해야 하는 방이면서 동시에 나중에 방문해야 되는 방인 경우는 없습니다.
    위 계획 중 2-2, 2-3, 2-4는 순서를 지켜 방문해야 하는 두 방의 쌍이 A → B(A를 먼저 방문하고 B를 방문함) 형태로 유일함을 의미합니다. 즉, 프로도는 아래와 같은 형태로 방문순서가 잡히지 않도록 방문 계획을 세웠습니다.
    
    A → B, A → C (방문순서 배열 order = [...,[A,B],...,[A,C],...]) 형태로 A를 방문 후에 방문해야 할 방이 B와 C로 두 개 또는 그 이상인 경우
    X → A, Z → A (방문순서 배열 order = [...,[X,A],...,[Z,A],...]) 형태로 A를 방문하기 전에 방문해야 할 방이 X와 Z로 두 개 또는 그 이상인 경우
    A → B → C (방문순서 배열 order = [...,[A,B],...,[B,C],...) 형태로 B처럼 A 방문 후이면서 동시에 C 방문 전인 경우
    그리고 먼저 방문해야 할 방과 나중에 방문할 방을 반드시 연속해서 방문해야 할 필요는 없어 A방을 방문한 후 다른 방을 방문한 후 B방을 방문해도 좋습니다.
    
    방 개수 n, 동굴의 각 통로들이 연결하는 두 방의 번호가 담긴 2차원 배열 path, 프로도가 정한 방문 순서가 담긴 2차원 배열 order가 매개변수로 주어질 때, 프로도가 규칙에 맞게 모든 방을 탐험할 수 있을 지 return 하도록 solution 함수를 완성해주세요.
    
    [제한사항]
    n은 2 이상 200,000 이하입니다.
    path 배열의 세로(행) 길이는 n - 1 입니다.
    path 배열의 원소는 [방 번호 A, 방 번호 B] 형태입니다.
    두 방 A, B사이를 연결하는 통로를 나타냅니다.
    통로가 연결하는 두 방 번호가 순서없이 들어있음에 주의하세요.
    order 배열의 세로(행) 길이는 1 이상 (n / 2) 이하입니다.
    order 배열의 원소는 [방 번호 A, 방 번호 B] 형태입니다.
    A번 방을 먼저 방문한 후 B번 방을 방문해야 함을 나타냅니다.

--- programmers/2020카카오인턴십/0505_dfs_동굴탐험.py
+++ programmers/2020카카오인턴십/0505_dfs_동굴탐험.py
@@ -1,94 +1,52 @@
-# https://taehee-kim-dev.tistory.com/116
-
 import sys
 sys.setrecursionlimit(10**6)
-
 from collections import defaultdict
-link_dict = defaultdict(list)   # 연결정보저장
-need_dict = defaultdict(list)   # a번 노드를 방문하기 위해 방문해야할 노드들 저장
 
+link_dict = defaultdict(list)
+need_dict = defaultdict(list)
 
-def solution(n, path, order):
-    for a, b in path:
+def solution(n,path,order):
+    for a,b in path:
         link_dict[a].append(b)
         link_dict[b].append(a)
+    # 역방향으로 그래프 체크
+    set_need_dict(0,-1)
 
-        # 0번 방에 유일한 입구 연결되어있으므로, 모든 방들은 방문되기 위해서 0번 방 무조건 거쳐야함
-        # 따라서 0번 방부터 순서를 따져봐야함
-        # 여기서 이전에 방문해야하는 방 번호인 -1은 의미없는 숫자임.
-    set_need_dict(0,-1) # link에서 부모-자식관계 파악해서 방문해야할 노드 설정
-
-    for a, b in order:
-        need_dict[b].append(a)  # 추가로 주어진 order에 따라 방문해야할 노드 설정
-
+    # order에 따라서도 역방향 체크
+    for a,b in order:
+        need_dict[b].append(a)
     visited = [False]*n
     recur = [False]*n
-    for i in range(n):
 
-        # i를 시작점으로 해서 사이클이 발생한다는 건 need_dict가 방문 역방향 순에 대한 정보이므로
-        # i를 방문하기 이전에 i를 방문해야한다는 뜻이 되는데
-        # 이는 말이 안되고, 결국 i를 방문할 수 없다는 의미이므로
-        # 모든 방을 방문할 수 없게 되니까 무조건 False를 반환
-        if is_cycle(i, visited, recur):
+    # 0번부터 n-1번까지 방번호(노드)가 사이클에 걸리는지 체크
+    for node in range(n):
+        if is_cycle(node, visited, recur):
             return False
     return True
 
-
-def set_need_dict(node, parent_node):
-    # node 이전에 방문해야 하는 방번호는 parent_node
-    # link_dict[node]에 해당하는 값은 node와 연결되어있는 모든 방들의 번호(노드들)를 원소로 갖는 리스트
-    # 이 리스트에서 node와 연결되어 있는 노드를 하나씩 꺼냄
-    for next_node in link_dict[node]:
-        if next_node == parent_node:
-            # 만약 연결되어있는 노드가
-            # node 이전에 방문해야하는 노드(parent_node)라면
-            # 필요없으므로 버리고 연결되어있는 다른 노드 꺼낸다.
-            continue
-
-        # 여기로 넘어왔으면,
-        # next_node는 node 이후에 방문해야 하는 노드임.
-        # 이후에 방문해야 하는 노드(next_node)가 더 이상 존재하지 않으면
-        # node는 리프노드(맨 아래 끝 노드)이므로 재귀가 끝나고 return 됨.
-        # {next_node: [node1, node2, ...]}
-
-        # 역방향 순서로 저장하는 과정임.
-        need_dict[next_node].append(node)
-        # next_node를 node로 해서 재귀호출
-        set_need_dict(next_node, node)
-
+# dfs로 사이클인지 아닌지 체크
 def is_cycle(node, visited, recur):
-    # dfs
-    # cycle이 존재한다면 어떤 노드를 방문하기 위해 그 노드를 방문해야한다는 모순 생기므로 False 리턴
-    # 예를 들어 만약 1이라는 노드가 부모 타고 올라가다가
-    # 어떤 노드의 부모가 자신인 1이게 되면 사이클이 존재한다는 의미이므로
-    # 모든 방을 탐방하지 못할 수도 있음
-    if visited[node]:   # 현재 검사하는 방 번호가 이미 방문한 방 번호라면 사이클이 발생한 것이므로 무조건 True 반환
+    if visited[node]:
         return True
-
-    # 현재 검사하는 방 번호부터 시작되는 검사(현재 방 번호 이하의 방 번호들에 대한 검사)가
-    # 사이클이 발생하지 않는다고 체크되어있으면 이 이후 검사는 어차피 False(사이클이 발생하지 않음)
     if recur[node]:
         return False
 
-    visited[node] = True # 방문했음 체크
+    visited[node] = True
     recur[node] = True
 
-    # node 이전에 방문해야 하는 방 번호(parent_node)를 하나씩 꺼낸다.
     for parent_node in need_dict[node]:
-        # parent_node가 사이클 생기는지 검사
         if is_cycle(parent_node, visited, recur):
             return True
-
-    # 방문 역순의 다음 순서가 더 이상 없을 때까지 계속 dfs
-    # 여기로 넘어왔으면, 현재 node부터 시작했을 때 사이클이 생기지 않음을 의미
-    # 따라서 node 이후의 역순 방문은 사이클이 발생하지 않음 체크
-    # 그리고 다음 dfs 검사에서 visited 재사용 위해 False로 다시 바꿔주기
-    # 사이클이 발생하지 않았음(False)를 반환
     visited[node] = False
     return False
 
 
-
+def set_need_dict(node, parent_node):
+    for next_node in link_dict[node]:
+        if next_node == parent_node:
+            continue
+        need_dict[next_node].append(node)
+        set_need_dict(next_node, node)
 
 
 if __name__ == '__main__':
@@ -96,5 +54,3 @@ if __name__ == '__main__':
                            (9, [[8,1],[0,1],[1,2],[0,7],[4,7],[0,3],[7,5],[3,6]], [[4,1],[5,2]]),
                            (9, [[0,1],[0,3],[0,7],[8,1],[3,6],[1,2],[4,7],[7,5]], [[4,1],[8,7],[6,5]])]:
         print(solution(n, path, order))
-
-
