59a09c171f46e3861767c9bf2fedbeebc723d4e1
jyshin0926 <sjy777star@gmail.com>
2021-05-07 01:08:18 +0900

    210506_DFS_경주로건설 https://programmers.co.kr/learn/courses/30/lessons/67259
    도면의 상태(0은 비어 있음, 1은 벽)을 나타내는 2차원 배열 board가 매개변수로 주어질 때, 경주로를 건설하는데 필요한 최소 비용을 return 하도록 solution 함수를 완성해주세요.
    
    [제한사항]
    board는 2차원 정사각 배열로 배열의 크기는 3 이상 25 이하입니다.
    board 배열의 각 원소의 값은 0 또는 1 입니다.
    도면의 가장 왼쪽 상단 좌표는 (0, 0)이며, 가장 우측 하단 좌표는 (N-1, N-1) 입니다.
    원소의 값 0은 칸이 비어 있어 도로 연결이 가능함을 1은 칸이 벽으로 채워져 있어 도로 연결이 불가능함을 나타냅니다.
    board는 항상 출발점에서 도착점까지 경주로를 건설할 수 있는 형태로 주어집니다.
    출발점과 도착점 칸의 원소의 값은 항상 0으로 주어집니다.

---
+++ programmers/2020카카오인턴십/0506_bfs_경주로건설.py
@@ -0,0 +1,54 @@
+import math
+from collections import deque
+def solution(board):
+    length = len(board)
+    directions = [(-1,0),(0,-1),(1,0),(0,1)]
+    ans = math.inf
+    queue = deque()
+    queue.append((0,0,-1,0))    # x좌표 y좌표 방향 비용
+    visit = {(0,0,0):0, (0,0,1):0, (0,0,2):0, (0,0,3):0}
+    while queue:
+        x,y,way,cost = queue.popleft()
+        for d in range(4):
+            nx = x + directions[d][0]
+            ny = y + directions[d][1]
+            if 0 <= nx < length and 0 <= ny < length and board[nx][ny] == 0:
+                new_cost = cost
+               # print('new_cost:',new_cost)
+
+                # 가장 처음에는 방향없으므로(-1) 비용에 100 추가
+                if way == -1:
+                    new_cost += 100
+
+                # 기존 방향과 진행 방향이 평행한 경우
+                elif (way-d) % 2 == 0:
+                    new_cost += 100
+
+                # 기존 방향과 진행 방향이 수직인 경우
+                else:
+                    new_cost += 600
+
+                # 목적지에 도착했을 경우 정답값과 비교하여 더 작은 것으로 정답값 갱신
+                if nx == length-1 and ny == length-1:
+                    ans = min(ans, new_cost)
+
+                # 기존에 방문하지 않았거나 방문했을 때보다 비용이 적을 경우에만 큐에 넣음
+                elif visit.get((nx,ny,d)) is None or visit.get((nx,ny,d)) > new_cost:
+                    visit[(nx,ny,d)] = new_cost
+                    queue.append((nx,ny,d,new_cost))
+    return ans
+
+if __name__ == '__main__':
+    for x in [[[0,0,0],[0,0,0],[0,0,0]], [[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,0,0,0,1],[0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0],[1,0,0,0,0,0,0,0]],
+              [[0,0,1,0],[0,0,0,0],[0,1,0,1],[1,0,0,0]],
+              [[0,0,0,0,0,0],[0,1,1,1,1,0],[0,0,1,0,0,0],[1,0,0,1,0,1],[0,1,0,0,0,1],[0,0,0,0,0,0]]]:
+        print(solution(x))
+
+# visit = {(0,0,0):0, (0,0,1):2, (0,0,2):0, (0,0,3):0}
+#
+# print(0%2)
+# print(-1%2)
+# print(-2%2)
+# print(-3%2)
+#
+# print(visit.get((0,0,0)))
