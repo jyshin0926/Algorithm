16ad82e2fbc002a410514ef87f7d9eaa5b54a999
jyshin0926 <sjy777star@gmail.com>
2021-05-18 12:42:22 +0900

    2105018_heap_디스크컨트롤러 https://leetcode.com/problems/maximal-square/
    문제 설명
    하드디스크는 한 번에 하나의 작업만 수행할 수 있습니다. 디스크 컨트롤러를 구현하는 방법은 여러 가지가 있습니다. 가장 일반적인 방법은 요청이 들어온 순서대로 처리하는 것입니다.

--- programmers/1122_힙_디스크컨트롤러.py
+++ programmers/1122_힙_디스크컨트롤러.py
@@ -1,21 +1,48 @@
-def solution(job):
-    cur = 0   # 현재 시점
-    jobs = sorted(job.copy(), key=lambda x:x[1])
-    time = 0
-    while len(jobs) != 0:
-        for i in range(len(jobs)):
-            if jobs[i][0] <= cur:
-                cur += jobs[i][1]
-                time += (cur-jobs[i][0])
-                del jobs[i]
+def solution(jobs):
+    job = sorted(jobs, key=lambda x:x[1])  # 소요 시간 기준으로 sorting
+    #print(job)
+    cur = 0; ans = 0
+    while len(job) > 0:
+        for i in range(len(job)):
+            if job[i][0] <= cur:
+                cur += job[i][1]
+                ans += (cur-job[i][0])
+                #print(ans)
+                job.pop(i)
                 break
-            elif i == len(jobs)-1:
+            elif i == len(job)-1:
                 cur += 1
-    return time//len(job)
+    return ans//len(jobs)
+
+# 풀이2 heapq
+import heapq
+def solution2(jobs):
+    ans, cur, cnt = 0,0,0
+    start = -1
+    heap = []
+    while len(jobs) > cnt:
+        for s,t in jobs:
+            if start < s <= cur:
+                # 힙에 push할 때는 작업의 소요 시간 기준으로 최소힙 만들어져야하므로
+                # jobs의 요소를 [소요시간, 요청시점]으로 앞뒤 바꿔서 넣어준다
+                heapq.heappush(heap, [t,s])
+        if len(heap) > 0:
+            tmp = heapq.heappop(heap)
+            start = cur
+            cur += tmp[0]
+            ans += (cur - tmp[1])
+            cnt += 1
+        else:
+            cur += 1
+    return ans//len(jobs)
 
 print(solution([[0, 3], [1, 9], [2, 6]]))
+print(solution([[24, 10], [28, 39], [43, 20], [37, 5], [47, 22], [20, 47], [15, 34], [15, 2], [35, 43], [26, 1]]
+))   # 72
 
-
+print(solution2([[0, 3], [1, 9], [2, 6]]))
+print(solution2([[24, 10], [28, 39], [43, 20], [37, 5], [47, 22], [20, 47], [15, 34], [15, 2], [35, 43], [26, 1]]
+))
 
 
 # 하드디스크는 한 번에 하나의 작업만 수행 가눙
