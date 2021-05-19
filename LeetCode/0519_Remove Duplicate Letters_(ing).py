f705f943c505cdecf24e9c69f6d7dfec5b08db83
jyshin0926 <sjy777star@gmail.com>
2021-05-19 22:37:47 +0900

    2105019_stack_Remove Duplicate Letters https://leetcode.com/problems/remove-duplicate-letters/
    Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
    
    Note: This question is the same as 1081: https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/

--- LeetCode/0519_Remove Duplicate Letters_(ing).py
+++ LeetCode/0519_Remove Duplicate Letters_(ing).py
@@ -1,26 +1,26 @@
-class Solution:
-    def removeDuplicateLetters(self, s: str) -> str:
-        tmp = []; s = list(s)
-        while len(s) > 0:
-            for i in range(len(s)):
-                if s[i] not in tmp:  # 최초 문자는 붙이기
-                    tmp.append(s.pop(i))
-                elif s[i] in tmp:
-                    if tmp[-1] > s[i]:
-                        #if s[i] == tmp[0]:
-                        for x in tmp:
-                            if x < s[i] and tmp.index(x) < tmp.index(s[i]):
-                                tmp.remove(s[i])
-                                tmp.append(s.pop(i))
-                        s.pop(i)
-                    else:
-                        tmp.remove(s[i])
-                        tmp.append(s.pop(i))
-                break
-        return ''.join(tmp)
-
 # class Solution:
 #     def removeDuplicateLetters(self, s: str) -> str:
+#         tmp = []; s = list(s)
+#         while len(s) > 0:
+#             for i in range(len(s)):
+#                 if s[i] not in tmp:  # 최초 문자는 붙이기
+#                     tmp.append(s.pop(i))
+#                 elif s[i] in tmp:
+#                     if tmp[-1] > s[i]:
+#                         #if s[i] == tmp[0]:
+#                         for x in tmp:
+#                             if x < s[i] and tmp.index(x) < tmp.index(s[i]):
+#                                 tmp.remove(s[i])
+#                                 tmp.append(s.pop(i))
+#                         s.pop(i)
+#                     else:
+#                         tmp.remove(s[i])
+#                         tmp.append(s.pop(i))
+#                 break
+#         return ''.join(tmp)
+
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
 #         tmp = []; s = list(s)
 #         while len(s) > 0:
 #             for i in range(len(s)):
@@ -38,17 +38,17 @@ class Solution:
 #                 break
 #         return ''.join(tmp)
 
-        # for x in sorted(set(s)):
-        #     print('sorted(set(s)):',sorted(set(s)))
-        #     print(x)
-        #     suf = s[s.index(x):]
-        #     print('suf:',set(suf))
-        #     print('set(s):',set(s))
-        #
-        #     if set(s) == set(suf):
-        #         print('update ans:', x + self.removeDuplicateLetters(suf.replace(x, '')),'\n')
-        #         return x + self.removeDuplicateLetters(suf.replace(x, ''))
-        # return ''
+        for x in sorted(set(s)):
+            #print('sorted(set(s)):',sorted(set(s)))
+            #print(x)
+            suf = s[s.index(x):]
+            #print('suf:',set(suf))
+            #print('set(s):',set(s))
+
+            if set(s) == set(suf):
+                #print('update ans:', x + self.removeDuplicateLetters(suf.replace(x, '')),'\n')
+                return x + self.removeDuplicateLetters(suf.replace(x, ''))
+        return ''
 
         # s 돌면서 이후에 같은 Letter 있으면 pop(0)
         # 체크하다가 안 나온 알파벳 다음에 그 앞에 나온 것들도 있는지 체크
