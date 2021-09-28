d831bd3f826e505b987f44f7f61185953ea96142
jyshin0926 <sjy777star@gmail.com>
2021-09-28 22:50:13 +0900

    Leetcode

--- LeetCode/비선형 자료구조/graph/332_Reconstruct Itinerary.py
+++ LeetCode/비선형 자료구조/graph/332_Reconstruct Itinerary.py
@@ -25,14 +25,14 @@ def findItinerary(tickets):
 # sol2. 스택을 활용한 일정 그래프 반복
 def findItinerary2(tickets):
     graph = defaultdict(list)
-    # 그래프 순서대로 구성
-    for a, b in sorted(tickets):
+    # 그래프 뒤집어서 구성
+    for a, b in sorted(tickets,reverse=True):
         graph[a].append(b)
     result, stack = [], ['JFK']
     while stack:
         # 반복으로 스택을 구성하되 막히는 부분에서 풀어내는 처리
         while graph[stack[-1]]:
-            stack.append(graph[stack[-1]].pop(0))
+            stack.append(graph[stack[-1]].pop())
         result.append(stack.pop())
 
     # 다시 뒤집어서 어휘 순 결과로 출력
